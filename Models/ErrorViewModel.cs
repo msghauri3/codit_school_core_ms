c6814ebc60a31e8fa7fb7c70c4b802a59dec9bcb
msghauri3 <msghauri3@yahoo.com>
2025-10-22 12:49:55 +0500

    Initial commit: ASP.NET Core project with proper .gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace codit_school_core_ms.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
